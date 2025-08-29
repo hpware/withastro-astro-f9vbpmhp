d07fec307161cbdd237a4a4fcd96d66dcaafaa34
null <null>
2025-08-29 21:49:00 +0800

    NEW

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+//import vue from '@astrojs/vue';
 
 
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()],
-      },
-      build: {
-        inlineStylesheets: 'always'
-      }
+  vite: {
+      plugins: [tailwindcss()],
+    },
+
+  build: {
+    inlineStylesheets: 'always',
+    inlineJavascript: 'always',
+    compressHTML: true
+  },
+
+  //integrations: [vue()]
 });
